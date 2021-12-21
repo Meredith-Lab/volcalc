e5ffeaf32cfaed9372ae7ff4b460bb73ab705f19
Kristina Riemer <kristina.riemer@weecology.org>
2021-12-21 11:51:20 -0700

    Make mol file download optional if file already exists; closes #4

--- R/save_compound_mol.R
+++ R/save_compound_mol.R
@@ -6,24 +6,26 @@
 #' @param compound_id character string that is 5 digits prepended with a "C"
 #' @param pathway_id character string that is 5 digits prepended with "map"
 #' @param path relative path to location to download data
+#' @param redownload download file again even if it has already been downloaded at path
 #'
 #' @return downloaded .mol file
 #' @export
 #' @importFrom magrittr %>%
 #' @importFrom utils write.table
-save_compound_mol <- function(compound_id, pathway_id, path) {
+save_compound_mol <- function(compound_id, pathway_id, path, redownload = FALSE) {
   . <- NULL
-  #dont redownload
   pathway_dir <- paste0(path, "/", pathway_id)
   if (!dir.exists(pathway_dir)) {
     dir.create(pathway_dir, recursive = TRUE)
   }
-  mol <- KEGGREST::keggGet(compound_id, option = "mol")
-  mol_clean <- gsub(">.*", "", mol) %>%
-    paste0("\n\n\n", .)
-  file_path <- paste0(pathway_dir, "/", compound_id, ".mol")
-  write.table(mol_clean,
-    file = file_path, row.names = FALSE,
-    col.names = FALSE, quote = FALSE
-  )
+  if (!file.exists(paste0(pathway_dir, "/", compound_id, ".mol")) | isTRUE(redownload)) {
+    mol <- KEGGREST::keggGet(compound_id, option = "mol")
+    mol_clean <- gsub(">.*", "", mol) %>%
+      paste0("\n\n\n", .)
+    file_path <- paste0(pathway_dir, "/", compound_id, ".mol")
+    write.table(mol_clean,
+      file = file_path, row.names = FALSE,
+      col.names = FALSE, quote = FALSE
+    )
+  }
 }
