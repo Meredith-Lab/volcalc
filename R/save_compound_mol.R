e1cea105133942778d43b550452e3459630a734e
Kristina Riemer <kristina.riemer@weecology.org>
2022-01-03 14:48:35 -0700

    Add compound formula as optional input to download mol file

--- R/save_compound_mol.R
+++ R/save_compound_mol.R
@@ -3,17 +3,37 @@
 #' Using KEGG ID values for compound and pathway, download
 #' and clean up corresponding mol file
 #'
-#' @param compound_id character string that is 5 digits prepended with a "C"
 #' @param pathway_id character string that is 5 digits prepended with "map"
 #' @param path relative path to location to download data
+#' @param compound_id character string that is 5 digits prepended with a "C"
+#' @param compound_formula character string of compound formula
 #' @param redownload download file again even if it has already been downloaded at path
 #'
 #' @return downloaded .mol file
 #' @export
 #' @importFrom magrittr %>%
 #' @importFrom utils write.table
-save_compound_mol <- function(compound_id, pathway_id, path, redownload = FALSE) {
-  . <- NULL
+save_compound_mol <- function(pathway_id, path, compound_id = NULL,
+                              compound_formula = NULL, redownload = FALSE) {
+  . <- kegg_id <- NULL
+  if (is.null(compound_id) & is.null(compound_formula)) {
+    stop("either compound_id or compound_formula needs to be specified")
+  }
+  if (!is.null(compound_formula)) {
+    formula_options <- KEGGREST::keggFind("compound", compound_formula, "formula") %>%
+      data.frame() %>%
+      tibble::rownames_to_column("kegg_id") %>%
+      dplyr::mutate(kegg_id = substr(kegg_id, 5, nchar(kegg_id))) %>%
+      dplyr::select(kegg_id)
+    if (nrow(formula_options) == 0) {
+      stop("compound_formula does not exist")
+    } else if (nrow(formula_options) == 1) {
+      compound_id <- formula_options$kegg_id[1]
+      message("single compound found for formula, will use KEGG ID ", compound_id)
+    } else {
+      stop("multiple compounds found for formula, choose from these options and rerun: ", paste(as.character(unique(formula_options$kegg_id)), collapse = ", "))
+    }
+  }
   if (!stringr::str_detect(compound_id, "^[C][:digit:]{5}$")) {
     stop("compound_id is not in the correct KEGG format")
   }
@@ -35,6 +55,3 @@ save_compound_mol <- function(compound_id, pathway_id, path, redownload = FALSE)
     )
   }
 }
-
-stringr::str_detect("C12345687", "[C][:digit:]{5}")
-stringr::str_detect("C12345fjdklsfjdsl", "[C][0-9]{5}")
--- tests/testthat/test-save_compound_mol.R
+++ tests/testthat/test-save_compound_mol.R
@@ -1,8 +1,25 @@
 test_that("file downloads with correct counts block", {
-  save_compound_mol("C16181", "map00361", "tests/testthat/data")
+  save_compound_mol("map00361", "tests/testthat/data", compound_id = "C16181", redownload = TRUE)
   ex_file_contents <- readLines("tests/testthat/data/map00361/C16181.mol")
   expect_equal(
     ex_file_contents[4],
     "12 12  0  0  1  0  0  0  0  0999 V2000"
   )
+})
+
+test_that("file downloads with formula input with correct counts block", {
+  save_compound_mol("map00361", "tests/testthat/data", compound_formula = "C6H4Cl4O2", redownload = TRUE)
+  ex_file_contents <- readLines("tests/testthat/data/map00361/C18238.mol")
+  expect_equal(
+    ex_file_contents[4],
+    "12 12  0  0  1  0  0  0  0  0999 V2000"
+  )
+})
+
+test_that("formula input with no KEGG compound ID match returns error", {
+  expect_error(save_compound_mol("map00361", "tests/testthat/data", compound_formula = "C200H200N200"))
+})
+
+test_that("formula input with multiple KEGG compound ID matches returns error", {
+  expect_error(save_compound_mol("map00623", "tests/testthat/data", compound_formula = "C7H10O5"))
 })
