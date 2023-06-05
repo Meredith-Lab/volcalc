c225260d766bc65b0f4f93f6340856d17799b95b
Eric R. Scott <scottericr@gmail.com>
2023-06-05 11:39:10 -0400

    use tempdir and skip_on_cran()

--- tests/testthat/test-save_compound_mol.R
+++ tests/testthat/test-save_compound_mol.R
@@ -1,7 +1,11 @@
+# These test that the download from the KEGG API is working.  Skipped on CRAN to
+# avoid problems if API is down.
 test_that("file downloads with correct counts block", {
-  save_compound_mol(compound_id = "C16181", path = "tests/testthat/data",
-                    redownload = TRUE)
-  ex_file_contents <- readLines("tests/testthat/data/C16181.mol")
+  skip_on_cran()
+  
+  out <- withr::local_tempdir()
+  save_compound_mol(compound_id = "C16181", path = out)
+  ex_file_contents <- readLines(file.path(out, "C16181.mol"))
   expect_equal(
     ex_file_contents[4],
     "12 12  0  0  1  0  0  0  0  0999 V2000"
@@ -9,9 +13,13 @@ test_that("file downloads with correct counts block", {
 })
 
 test_that("file downloads into pathway subfolder with correct counts block", {
-  save_compound_mol(compound_id = "C16181", pathway_id = "map00361",
-                    path = "tests/testthat/data",  redownload = TRUE)
-  ex_file_contents <- readLines("tests/testthat/data/map00361/C16181.mol")
+  skip_on_cran()
+  
+  out <- withr::local_tempdir()
+  save_compound_mol(compound_id = "C16181",
+                    pathway_id = "map00361",
+                    path = out)
+  ex_file_contents <- readLines(file.path(out, "map00361/C16181.mol"))
   expect_equal(
     ex_file_contents[4],
     "12 12  0  0  1  0  0  0  0  0999 V2000"
@@ -19,9 +27,13 @@ test_that("file downloads into pathway subfolder with correct counts block", {
 })
 
 test_that("file downloads with formula input with correct counts block", {
-  save_compound_mol(compound_formula = "C6H4Cl4O2", pathway_id = "map00361",
-                    path = "tests/testthat/data", redownload = TRUE)
-  ex_file_contents <- readLines("tests/testthat/data/map00361/C18238.mol")
+  skip_on_cran()
+  
+  out <- withr::local_tempdir()
+  save_compound_mol(compound_formula = "C6H4Cl4O2",
+                    pathway_id = "map00361",
+                    path = out)
+  ex_file_contents <- readLines(file.path(out, "map00361/C18238.mol"))
   expect_equal(
     ex_file_contents[4],
     "12 12  0  0  1  0  0  0  0  0999 V2000"
@@ -29,11 +41,25 @@ test_that("file downloads with formula input with correct counts block", {
 })
 
 test_that("formula input with no KEGG compound ID match returns error", {
-  expect_error(save_compound_mol(compound_formula = "C200H200N200", pathway_id = "map00361",
-                                 path = "tests/testthat/data"))
+  skip_on_cran()
+  
+  expect_error(
+    save_compound_mol(
+      compound_formula = "C200H200N200",
+      pathway_id = "map00361",
+      path = withr::local_tempdir()
+    )
+  )
 })
 
 test_that("formula input with multiple KEGG compound ID matches returns error", {
-  expect_error(save_compound_mol(compound_formula = "C7H10O5", pathway_id = "map00623",
-                                 path = "tests/testthat/data"))
+  skip_on_cran()
+  
+  expect_error(
+    save_compound_mol(
+      compound_formula = "C7H10O5",
+      pathway_id = "map00623",
+      path = withr::local_tempdir()
+    )
+  )
 })
