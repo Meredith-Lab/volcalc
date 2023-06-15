37f260f503390ecea24e601ebd36a6c93431a278
Eric R. Scott <scottericr@gmail.com>
2023-06-15 14:26:07 -0400

    move and rename placehold function

--- R/utils.R
+++ R/utils.R
@@ -1,1 +1,6 @@
 `%+%` <- function(x, y)  mapply(sum, x, y, MoreArgs = list(na.rm = TRUE))
+
+#This only exists to silence a R CMD check note about ChemmineOB being in Imports but no functions being used in volcalc directly.
+zzz <- function() {
+  ChemmineOB::prop_OB
+}
