9af9e3b35bed7034813f36c50724575d1d31fb73
Eric R. Scott <scottericr@gmail.com>
2023-06-16 16:42:31 -0400

    cleanup check notes

--- NEWS.md
+++ NEWS.md
@@ -1,5 +1,7 @@
 # volcalc (development version)
 
+- Addition of `get_mol_kegg()` which will eventually replace `save_compound_mol`
+
 # volcalc 1.0.0
 
 * Initial release of `volcalc`.  This is the version of the code used in an in-prep manuscript.  Version 2.0.0 will include **breaking changes**
--- R/get_mol_kegg.R
+++ R/get_mol_kegg.R
@@ -3,8 +3,10 @@
 #' Downloads mol files corresponding to individual compounds or compounds in a
 #' pathway from KEGG.
 #'
-#' @param compound_ids character vector of KEGG compound IDs---5 digits prepended with a "C".
-#' @param pathway_ids character vector of KEGG pathway IDs---5 digits prepended with "map".
+#' @param compound_ids character vector of KEGG compound IDs---5 digits
+#'   prepended with a "C".
+#' @param pathway_ids character vector of KEGG pathway IDs---5 digits prepended
+#'   with "map".
 #' @param dir path to a folder to save mol files in.
 #'
 #' @returns a tibble with the columns `compound_ids`, `pathway_ids` (if used),
@@ -17,6 +19,9 @@
 #' get_mol_kegg(pathway_ids = "map00253", dir = tempdir())
 #' }
 get_mol_kegg <- function(compound_ids, pathway_ids, dir){
+  
+  #TODO: implement redownload=FALSE arg
+  
   if(missing(dir)) stop("`dir` is required")
   if ((missing(compound_ids) & missing(pathway_ids)) |
       !missing(compound_ids) & !missing(pathway_ids)) {
@@ -31,7 +36,7 @@ get_mol_kegg <- function(compound_ids, pathway_ids, dir){
     fs::dir_create(dir)
     out_tbl <-
       tibble::tibble(compound_id = compound_ids) %>% 
-      dplyr::mutate(mol_path = fs::path(dir, compound_id, ext = "mol"))
+      dplyr::mutate(mol_path = fs::path(dir, .data$compound_id, ext = "mol"))
   }
   # if pathways are provided
   if (!missing(pathway_ids)) {
@@ -44,7 +49,7 @@ get_mol_kegg <- function(compound_ids, pathway_ids, dir){
     out_tbl <- 
       tibble::enframe(compound_ids_list, name = "pathway_id", value = "compound_id") %>% 
       tidyr::unnest(tidyselect::everything()) %>% 
-      dplyr::mutate(mol_path = fs::path(dir, pathway_id, compound_id, ext = "mol"))
+      dplyr::mutate(mol_path = fs::path(dir, .data$pathway_id, .data$compound_id, ext = "mol"))
     
   }
   
@@ -57,14 +62,13 @@ get_mol_kegg <- function(compound_ids, pathway_ids, dir){
     mol <- KEGGREST::keggGet(compound_id, option = "mol")
     
     # Adds title to mol file because it is used later on by get_fx_groups()
-    # currently get_fx_groups() queries KEGG for the title, but need to wean it
-    # of KEGG API
+    # TODO: currently get_fx_groups() queries KEGG for the title, but need to
+    # wean it of KEGG API
     names <- KEGGREST::keggGet(compound_id)[[1]]$NAME
     #just use the first name and remove separator
     title <- stringr::str_remove(names[1], ";")
     #add title line to mol file
-    mol_clean <- gsub(">.*", "", mol) %>%
-      paste0(title, "\n\n", .)
+    mol_clean <- paste0(title, "\n\n", gsub(">.*", "", mol))
     mol_clean
   }
   mols <- lapply(out_tbl$compound_id, .get_mol_kegg)
