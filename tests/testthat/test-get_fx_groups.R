4e35b74a0c52e40b2e08912198277f8988b4f254
Eric R. Scott <scottericr@gmail.com>
2023-06-26 18:51:40 -0700

    modify tests for new version of get_fx_groups()

--- tests/testthat/test-get_fx_groups.R
+++ tests/testthat/test-get_fx_groups.R
@@ -1,10 +1,9 @@
 test_that("a functional group count is correct for example compound", {
-  ex_df <- get_fx_groups(compound_id = "C16181", path = "data")
+  sdf <- ChemmineR::read.SDFset("data/C16181.mol")
+  ex_df <- get_fx_groups(sdf)
   expect_equal(ex_df$hydroxyl_groups, 1)
 })
 
-test_that("functional group count is correct for example compound with specified pathway", {
-  ex_df <- get_fx_groups(compound_id = "C16181", pathway_id = "map00361",
-                         path = "data")
-  expect_equal(ex_df$hydroxyl_groups, 1)
-})
+#' TODO:
+#' - Additional correctness tests
+#' - Test that compound name is read correctly or is NA when empty
