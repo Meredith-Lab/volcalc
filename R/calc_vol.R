fdf2c612455650ddf96a27af006704ad44582292
Eric R. Scott <scottericr@gmail.com>
2023-06-28 13:21:00 -0700

    refactor calc_vol() to start with .mol file

--- R/calc_vol.R
+++ R/calc_vol.R
@@ -3,19 +3,8 @@
 #' Volatility value and category is estimated for specified compound using the
 #' SIMPOL formula
 #'
-#' @param compound_id A character string that is 5 digits prepended with a "C".
-#' @param compound_formula A character string detailing a compound formula.
-#' @param pathway_id An optional character string specifying KEGG pathway ID, in
-#'   format of 5 digits prepended with "map".
-#' @param path An optional parameter to set relative path to location to
-#'   download data.
-#' @param save_file Whether to save downloaded compound mol files.
-#' @param redownload Download file again even if it has already been downloaded
-#'   at path.
-#' @param get_groups When `FALSE`, will expect a dataframe to be read in with
-#'   `fx_groups_df` argument.
-#' @param fx_groups_df A dataframe of functional group counts for compounds
-#'   generated from [`get_fx_groups()`].
+#' @param input a path to a .mol file or a character representation such as SMILES or InChI 
+#' @param from the form of `input` (currently only a path to a .mol file is implemented)
 #' @param return_fx_groups When `TRUE`, includes functional group counts in
 #'   final dataframe.
 #' @param return_calc_steps When `TRUE`, includes intermediate volatility
@@ -24,49 +13,33 @@
 #' @return Dataframe with columns of basic compound info and volatility value
 #'   and category. See documentation for column descriptions.
 #'
-#' @examples
-#' \dontrun{
-#' ex_compound <- calc_vol(compound_id = "C16181")
-#' }
 #' @export
 calc_vol <-
-  function(compound_id = NULL,
-           compound_formula = NULL,
-           pathway_id = NULL,
-           path = "data",
-           redownload = FALSE,
-           save_file = TRUE,
-           get_groups = TRUE,
-           fx_groups_df = NULL,
+  function(input, 
+           from = c("mol_path"),
            return_fx_groups = FALSE,
            return_calc_steps = FALSE) {
     
-  if (is.null(compound_id) & is.null(compound_formula)) {
-    stop("either compound_id or compound_formula needs to be specified")
+  from <- match.arg(from)
+  
+  #TODO: vectorize so `input` can be a vector
+  if(from == "mol_path") {
+    compound_sdf <- ChemmineR::read.SDFset(input)
   }
-  if ((!isTRUE(save_file) & is.null(fx_groups_df)) | (!isTRUE(get_groups) & is.null(fx_groups_df)) |
-      (!isTRUE(save_file) & !isTRUE(get_groups) & is.null(fx_groups_df))) {
-    stop("either read in functional groups dataframe or set save_file and get_groups to true")
-  }
-  if (isTRUE(save_file)) {
-    save_compound_mol(compound_id, compound_formula, pathway_id, path, redownload)
-  }
-  if (isTRUE(get_groups)) {
-    fx_groups_df <- get_fx_groups(compound_id, pathway_id, path)
-  }
-    # calculate volatility
-    vol_df <- simpol1(fx_groups_df) 
+  fx_groups_df <- get_fx_groups(compound_sdf)
+  # calculate volatility
+  vol_df <- simpol1(fx_groups_df) 
   
   if (isTRUE(return_fx_groups) & !isTRUE(return_calc_steps)){
     subset_vol_df <-
-      dplyr::select(vol_df, pathway:name, volatility:category, carbons:fluorines)
+      dplyr::select(vol_df, formula:name, volatility:category, carbons:fluorines)
   } else if (!isTRUE(return_fx_groups) & isTRUE(return_calc_steps)){
     subset_vol_df <- 
-      dplyr::select(vol_df, pathway:name, volatility:category, mass, log_alpha:log_Sum)
+      dplyr::select(vol_df, formula:name, volatility:category, mass, log_alpha:log_Sum)
   } else if (isTRUE(return_fx_groups) & isTRUE(return_calc_steps)) {
     subset_vol_df <- vol_df
   } else {
-    subset_vol_df <- dplyr::select(vol_df, pathway:name, volatility:category)
+    subset_vol_df <- dplyr::select(vol_df, formula:name, volatility:category)
   }
   return(subset_vol_df)
   }
@@ -86,9 +59,8 @@ simpol1 <- function(fx_groups) {
     # table that used 293.15K (table 6 in Pankow & Asher 2008).  There is also a
     # table of coefs and equation to calculate b_k(T) at different values of T.
     # Why wasn't this implemented?
-    temp_K <- 293
     dplyr::mutate(
-      log_alpha = log((1000000 * mass) / (0.0000821 * temp_K), base = 10),
+      log_alpha = log((1000000 * mass) / (0.0000821 * 293), base = 10),
       # multiplier for each functional group is volatility contribution
       log_Sum = #TODO why is this called log_Sum?  There's no log operation, right?
         #TODO deal with NAs before this step and get rid of %+% operator
--- tests/testthat/test-calc_vol.R
+++ tests/testthat/test-calc_vol.R
@@ -1,37 +1,13 @@
 test_that("volatility estimate is correct for example compound for entire workflow", {
-  ex_vol_df <- calc_vol(compound_id = "C16181", path = "data", redownload = FALSE)
-  expect_equal(round(ex_vol_df$volatility, 6), 6.975571)
+  ex_vol_df <- calc_vol("data/C16181.mol")
+  expect_equal(ex_vol_df$volatility, 6.975571, tolerance = 1e-4)
 })
 
-test_that("volatility estimate is correct for example compound with pathway for entire workflow", {
-  ex_vol_df <- calc_vol(compound_id = "C16181", pathway_id = "map00361",
-                        path = "data", redownload = FALSE)
-  expect_equal(round(ex_vol_df$volatility, 6), 6.975571)
-})
-
-test_that("volatility estimate is correct for example compound from functional groups dataframe", {
-  ex_fx_df <- get_fx_groups(compound_id = "C16181", pathway_id = "map00361",
-                            path = "data")
-  ex_vol_df <- calc_vol(compound_id = "C16181", pathway_id = "map00361",
-                        path = "data", redownload = FALSE,
-                        get_groups = FALSE, fx_groups_df = ex_fx_df)
-  expect_equal(round(ex_vol_df$volatility, 6), 6.975571)
-})
-
-test_that("returns error if no functional groups dataframe and no saving file and getting functional groups dataframe", {
-  expect_error(calc_vol(compound_id = "C16181", pathway_id = "map00361",
-                        path = "data", redownload = FALSE,
-                        get_groups = FALSE, fx_groups_df = NULL))
-})
 
 test_that("returns correct number of columns depending on return arguments", {
-  expect_equal(ncol(calc_vol(compound_id = "C16181", pathway_id = "map00361",
-                             path = "data", redownload = FALSE)), 6)
-  expect_equal(ncol(calc_vol(compound_id = "C16181", pathway_id = "map00361",
-                             path = "data", return_fx_groups = TRUE, redownload = FALSE)), 45)
-  expect_equal(ncol(calc_vol(compound_id = "C16181", pathway_id = "map00361",
-                             path = "data", return_calc_steps = TRUE, redownload = FALSE)), 9)
-  expect_equal(ncol(calc_vol(compound_id = "C16181", pathway_id = "map00361",
-                             path = "data", return_fx_groups = TRUE,
-                             return_calc_steps = TRUE, redownload = FALSE)), 48)
+  expect_equal(ncol(calc_vol("data/C16181.mol")), 4)
+  expect_equal(ncol(calc_vol("data/C16181.mol", return_fx_groups = TRUE)), 43)
+  expect_equal(ncol(calc_vol("data/C16181.mol", return_calc_steps = TRUE)), 7)
+  expect_equal(ncol(calc_vol("data/C16181.mol", return_fx_groups = TRUE,
+                             return_calc_steps = TRUE)), 46)
 })
