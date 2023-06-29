c9e6316ca097b0b04f1a0074aff80526eda8e6bc
Eric R. Scott <scottericr@gmail.com>
2023-06-29 14:59:01 -0700

    remove calc_pathway_vol

--- tests/testthat/test-calc_pathway_vol.R
+++
@@ -1,5 +0,0 @@
-# test_that("average volatility is correct for all compounds in a pathway ", {
-#   skip_on_cran()
-#   ex_df <- calc_pathway_vol("map00361", path = withr::local_tempdir())
-#   expect_equal(round(mean(ex_df$volatility), 3), 4.522)
-# })
