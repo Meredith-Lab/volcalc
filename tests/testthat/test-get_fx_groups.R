c46729b72364a88f34954f5d0fd5a8c0c991e844
Eric R. Scott <scottericr@gmail.com>
2023-07-05 16:19:26 -0400

    example correctness test

--- tests/testthat/test-get_fx_groups.R
+++ tests/testthat/test-get_fx_groups.R
@@ -12,3 +12,32 @@ test_that("error with SDFset with more than one molecule", {
 #' TODO:
 #' - Additional correctness tests
 #' - Test that compound name is read correctly or is NA when empty
+
+test_that("get_fx_groups() distinguishes between ROOR, ROOH, and ROH", {
+  skip() #no pattern for hydroperoxide yet
+  #https://en.wikipedia.org/wiki/Tert-Butyl_hydroperoxide
+  hydroperoxide <- ChemmineR::smiles2sdf("CC(C)(C)OO")
+  
+  #https://en.wikipedia.org/wiki/Dicumyl_peroxide
+  peroxide <- ChemmineR::smiles2sdf("CC(C)(C1=CC=CC=C1)OOC(C)(C)C2=CC=CC=C2") 
+  
+  #https://en.wikipedia.org/wiki/Glucose
+  alcohol <- ChemmineR::smiles2sdf("OC[C@H]1OC(O)[C@H](O)[C@@H](O)[C@@H]1O")
+  
+  rooh <- get_fx_groups(hydroperoxide)
+  roor <- get_fx_groups(peroxide)
+  roh  <- get_fx_groups(alcohol)
+  
+  expect_equal(rooh$hydroperoxide, 1)
+  expect_equal(rooh$peroxide, 0) #don't double-count peroxides and hydroperoxides
+  expect_equal(rooh$hydroxyl_groups, 0)
+  
+  expect_equal(roor$peroxide, 1)
+  expect_equal(roor$hydroxyl_groups, 0)
+  expect_equal(roor$hydroperoxide, 0)
+  
+  expect_equal(roh$hydroxyl_groups, 5)
+  expect_equal(roh$peroxide, 0)
+  expect_equal(roh$hydroperoxide, 0)
+  
+})
