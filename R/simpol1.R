c2609e6f0dd83a131c7b02c5d3bde56eba21035d
Eric R. Scott <scottericr@gmail.com>
2023-07-11 09:39:45 -0400

    documentation for simpol

--- R/simpol1.R
+++ R/simpol1.R
@@ -6,15 +6,18 @@ utils::globalVariables(".data")
 #' will not usually use this function directly, but rather through [calc_vol()]
 #' which uses this as the default (currently only) method.
 #' 
-#'
+#' @details The method described in Pankow & Asher (2008) allows for
+#' calculations of logP at different temperatures.  This implementation
+#' currently only calculates values at 20ºC.
+#' 
 #' @param fx_groups a data.frame or tibble with counts of functional groups
 #'   produced by [get_fx_groups()] (or manually, with the same column names)
 #'
-#' @return The `fx_groups` tibble with the additional columns:
-#'   * `log_alpha` — not sure! units?
-#'   * `log_Sum` — \eqn{\sum_k\nu_{k,i}b_k(T)}, or the sum of the counts of functional groups (\eqn{\nu_{k,i}}) times the coefficients for each functional group (\eqn{b_K(T)}). units?
-#'   * `volatility` — relative volatility? units?
-#'   * `category` — category of volatility?
+#' @return The `fx_groups` tibble with an additional column for `log10_P`.
+#'   \eqn{\textrm{log}{10} P_{\textrm{L},i}^\circ(T) = \sum_k\nu_{k,i}b_k(T)},
+#'   or the sum of the counts of functional groups (\eqn{\nu_{k,i}}) times the
+#'   coefficients for each functional group (\eqn{b_K(T)}). Units are in log10
+#'   atmospheres.
 #'
 #' @references Pankow, J.F., Asher, W.E., 2008. SIMPOL.1: a simple group
 #'   contribution method for predicting vapor pressures and enthalpies of
@@ -29,18 +32,16 @@ utils::globalVariables(".data")
 #' fx_groups <- get_fx_groups(sdf)
 #' simpol1(fx_groups)
 simpol1 <- function(fx_groups) {
+  #TODO: this is actually a *modified* version of SIMPOL.1 with additional functional group coefs added.  Should there be a separate function for the original SMIPOL.1 method that doesn't count these additional groups
   fx_groups %>%
     # assume NAs are 0s for the sake of this calculation
     dplyr::mutate(dplyr::across(dplyr::where(is.integer), function(x)
       ifelse(is.na(x), 0L, x))) %>%
-    
-    
     # TODO: There is also a table of coefs and equation to calculate b_k(T) at
     # different values of T. Why wasn't this implemented?
     dplyr::mutate(
-      # multiplier for each functional group is volatility contribution
-      log_Sum = #TODO why is this called log_Sum?  There's no log operation, right?
-        #TODO I think log_Sum should include `constant`, as it is equivalent to b_0(T) * 1
+      # multiplier for each functional group is volatility contribution to log10P
+      log10_P = 
         # b_k(T)  * v_k,i
         (1.79     * 1) + #b_0(T) is an intercept/constant
         (-0.438   * .data$carbons) +
