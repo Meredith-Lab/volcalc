31b514390df230919f04cacb77e35f32237b4e4c
Eric R. Scott <scottericr@gmail.com>
2023-07-28 11:25:45 -0400

    add test for phenols

--- tests/testthat/test-get_fx_groups.R
+++ tests/testthat/test-get_fx_groups.R
@@ -9,8 +9,18 @@ test_that("functional group count is correct for example compound with specified
   expect_equal(ex_df$hydroxyl_groups, 1)
 })
 
-test_that("number of rings is correct", {
+test_that("correct number of rings", {
   ex_df <- get_fx_groups(compound_id = "C07481", path = "data")
+  bpa <- get_fx_groups(compound_id = "C13624", path = "data")
   expect_equal(ex_df$rings_aromatic, 2)
   expect_equal(ex_df$rings, 0)
+  expect_equal(bpa$rings_aromatic, 2)
+  expect_equal(bpa$rings, 0)
+})
+
+test_that("correct number of aromatic hydroxyl", {
+  bpa <- get_fx_groups(compound_id = "C13624", path = "data")
+  expect_equal(bpa$hydroxyl_groups, 0)
+  expect_equal(bpa$phenol, 2)
+  
 })
