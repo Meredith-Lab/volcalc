a394e012a37eac0881cb3fe368c1f736d5ba6486
Eric Scott <scottericr@gmail.com>
2023-10-23 10:40:27 -0400

    precompute kegg vignette

--- R/release_bullets.R
+++ R/release_bullets.R
@@ -1,6 +1,7 @@
 #works with usethis::use_release_issue()
 release_bullets <- function() {
   c(
-    "Increment version in CITATION.cff and CITATION"
+    "Increment version in CITATION.cff and CITATION",
+    "run vignettes/precompute.R script to precompute KEGG vignette"
   )
 }
