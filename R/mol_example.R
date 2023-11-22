a9c674a3804def9fcdb1c1c114d9ea0f4da8511c
Eric Scott <scottericr@gmail.com>
2023-11-22 13:00:05 -0700

    Just return all file paths so things like calc_vol(mol_example()) are possible

--- R/mol_example.R
+++ R/mol_example.R
@@ -3,8 +3,6 @@
 #' `volcalc` comes bundled with some example .mol files in its `inst/extdata`
 #' directory.  This function provides easy access to them.
 #'
-#' @param path Name of file.  If `NULL` (default), the options for example .mol
-#'   files will be listed instead.
 #' @details File names are the KEGG identifiers.  Compound names are as follows:
 #' - C00031: D-Glucose
 #' - C08491: (-)-Jasmonic acid
@@ -15,16 +13,12 @@
 #' @export
 #'
 #' @examples
-#' #list all examples
+#' #return paths to all example .mol files
 #' mol_example()
 #' 
-#' #return path to specific example file
-#' mol_example("C16181.mol")
-mol_example <- function(path = NULL) {
-  if (is.null(path)) {
-    dir(system.file("extdata", package = "volcalc"))
-  } else {
-    system.file("extdata", path, package = "volcalc", mustWork = TRUE)
-  }
+#' #examine the contents of a file
+#' readLines(mol_example()[1])
+mol_example <- function() {
+  dir(system.file("extdata", package = "volcalc"), full.names = TRUE)
 }
 
