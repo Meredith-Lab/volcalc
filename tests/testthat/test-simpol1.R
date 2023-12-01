46ea18a3962d14671a9585e6fe2617735cd27200
Eric Scott <scottericr@gmail.com>
2023-12-01 14:38:25 -0700

    add test for #91

---
+++ tests/testthat/test-simpol1.R
@@ -0,0 +1,17 @@
+test_that("isoprene is more volatile than glucose", {
+  isoprene <- ChemmineR::read.SDFset(mol_example()[5])
+  glucose  <- ChemmineR::read.SDFset(mol_example()[1])
+  
+  log10P_together <- simpol1(dplyr::bind_rows(
+    get_fx_groups(isoprene),
+    get_fx_groups(glucose)
+  ))$log10_P
+  
+  log10P_separate <- c(
+    simpol1(get_fx_groups(isoprene))$log10_P,
+    simpol1(get_fx_groups(glucose))$log10_P
+  )
+  
+  expect_gt(log10P_together[1], log10P_together[2])
+  expect_equal(log10P_together, log10P_separate)
+})
